9fc7165a10224f3b1a742c68e1b5b4d17efd224d
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-30 22:30:19 +0100

    Add a couple of import aliases.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: "@helpers", replacement: "/src/Helpers" },
+      { find: "@components", replacement: "/src/Components" },
+      { find: "@style", replacement: "/src/Styles" },
+      // {find:'',replacement:''},
+    ],
+  },
+});
