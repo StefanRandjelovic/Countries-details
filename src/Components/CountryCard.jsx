08dcbd309884c4f9c71c1d407d8ed8b1f214ad05
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-30 22:33:12 +0100

    Create CountryCard component for data rendering.

---
+++ src/Components/CountryCard.jsx
@@ -0,0 +1,45 @@
+// STYLES
+import "@style/CountryCard.scss";
+
+const CountryCard = ({ country }) => {
+  return (
+    <div className="country-card">
+      <h1>Country: {country.name.common}</h1>
+      <div className="innerInfo">
+        <p className="capital">
+          {country.capital && Object.values(country.capital).length > 1
+            ? "Capital cities: "
+            : "Capital city: "}{" "}
+          {country.capital ? Object.values(country.capital).join(", ") : "N/A"}
+        </p>
+        <p className="language">
+          {country.languages && Object.values(country.languages).length > 1
+            ? "Languages: "
+            : "Language: "}
+          {country.languages
+            ? Object.values(country.languages).join(", ")
+            : "N/A"}
+        </p>
+        <p className="population">
+          Population: {country.population.toLocaleString()}
+        </p>
+        <p className="area">
+          Area: {country.area.toLocaleString()} km<sup>2</sup>
+        </p>
+        <p className="currency">
+          {country.currencies &&
+          Object.values(country.currencies).map((currency) => currency.name)
+            .length > 1
+            ? "Curencies: "
+            : "Curency: "}
+          {country.currencies &&
+            Object.values(country.currencies)
+              .map((currency) => currency.name)
+              .join(", ")}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default CountryCard;
