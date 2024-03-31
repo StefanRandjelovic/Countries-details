6a7771bb1f02c40ec932411fcb6dd120d7feb974
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-31 21:35:12 +0200

    Add handleTextInput function.

--- src/Helpers/helper_functions.js
+++ src/Helpers/helper_functions.js
@@ -2,6 +2,10 @@
 import axios from "axios";
 
 export const handleFetch = async () => {
-    const { data } = await axios.get("https://restcountries.com/v3.1/all");
-    return data;
-  };
+  const { data } = await axios.get("https://restcountries.com/v3.1/all");
+  return data;
+};
+
+export const handleTextInput = (event, setInputValue) => {
+  setInputValue(event.target.value);
+};
