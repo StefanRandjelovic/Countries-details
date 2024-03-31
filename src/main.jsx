8f0181e7a64f0c1f2b03afa4b392b738a5a11125
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-31 21:32:10 +0200

    Import global styles file.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
+// STYLES
+import "@style/global.scss";
+
 // DEV DEPENDENCIES
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
