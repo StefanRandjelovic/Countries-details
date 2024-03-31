86bb08eae9df93a930c6d997a256cc72d4d810ca
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-31 21:33:32 +0200

    Refactor code and add countries currency and flag.

--- src/Components/CountryCard.jsx
+++ src/Components/CountryCard.jsx
@@ -6,37 +6,47 @@ const CountryCard = ({ country }) => {
     <div className="country-card">
       <h1>Country: {country.name.common}</h1>
       <div className="innerInfo">
-        <p className="capital">
-          {country.capital && Object.values(country.capital).length > 1
-            ? "Capital cities: "
-            : "Capital city: "}{" "}
-          {country.capital ? Object.values(country.capital).join(", ") : "N/A"}
-        </p>
-        <p className="language">
-          {country.languages && Object.values(country.languages).length > 1
-            ? "Languages: "
-            : "Language: "}
-          {country.languages
-            ? Object.values(country.languages).join(", ")
-            : "N/A"}
-        </p>
-        <p className="population">
-          Population: {country.population.toLocaleString()}
-        </p>
-        <p className="area">
-          Area: {country.area.toLocaleString()} km<sup>2</sup>
-        </p>
-        <p className="currency">
-          {country.currencies &&
-          Object.values(country.currencies).map((currency) => currency.name)
-            .length > 1
-            ? "Curencies: "
-            : "Curency: "}
-          {country.currencies &&
-            Object.values(country.currencies)
-              .map((currency) => currency.name)
-              .join(", ")}
-        </p>
+        <div className="innerText">
+          <p className="capital">
+            {country.capital && Object.values(country.capital).length > 1
+              ? "Capital cities: "
+              : "Capital city: "}{" "}
+            {country.capital
+              ? Object.values(country.capital).join(", ")
+              : "N/A"}
+          </p>
+          <p className="language">
+            {country.languages && Object.values(country.languages).length > 1
+              ? "Languages: "
+              : "Language: "}
+            {country.languages
+              ? Object.values(country.languages).join(", ")
+              : "N/A"}
+          </p>
+          <p className="population">
+            Population: {country.population.toLocaleString()}
+          </p>
+          <p className="area">
+            Area: {country.area.toLocaleString()} km<sup>2</sup>
+          </p>
+          <p className="currency">
+            {country.currencies &&
+            Object.values(country.currencies).map((currency) => currency.name)
+              .length > 1
+              ? "Curencies: "
+              : "Curency: "}
+            {country.currencies
+              ? Object.values(country.currencies)
+                  .map((currency) => currency.name)
+                  .join(", ")
+              : "N/A"}
+          </p>
+        </div>
+        <img
+          className="flag"
+          src={country.flags && country.flags.svg}
+          alt={country.flags && country.flags.alt}
+        />
       </div>
     </div>
   );
