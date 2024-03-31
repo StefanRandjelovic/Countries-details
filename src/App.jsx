67da2756c5cb198f4aa96595b4ff2e8e7c51f4f4
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-03-31 21:31:53 +0200

    Create rendering sections and implement search filter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,116 @@
 // DEV DEPENDENCIES
 import { useQuery } from "@tanstack/react-query";
-import { useEffect, useState } from "react";
 
 // HELPER FUNCTIONS
 import { handleFetch } from "@helpers/helper_functions";
 
 // COMPONENTS
 import CountryCard from "@components/CountryCard";
+import NavBar from "@components/NavBar";
+
+// GLOBAL VARIABLE
+import { searchFilter } from "@store/zustandStore";
 
 function App() {
-  const [countries, setCountries] = useState("");
+  // GLOBAL STATE
+  const { inputValue, setInputValue } = searchFilter();
 
   // FETCH COUNTRIES
-  const { data, isError, isLoading } = useQuery({
+  const { data, isError, isLoading, error } = useQuery({
     queryKey: ["countries-data"],
     queryFn: handleFetch,
   });
 
-  useEffect(() => {
-    data && localStorage.setItem("countries", JSON.stringify(data));
-    setCountries(JSON.parse(localStorage.getItem("countries")));
-  }, [data]);
-
-  // countries &&
-  //   countries.map(
-  //     (country) =>
-  //       country.currencies &&
-  //       console.log(
-  //         Object.values(country.currencies).map((currency) => currency.name)
-  //       )
-  //   );
+  console.log(data);
+  const dataToRender =
+    data &&
+    data.filter((country) => {
+      if (inputValue === "") {
+        return country;
+      } else {
+        return country.name.common
+          .toLowerCase()
+          .includes(inputValue.toLowerCase());
+      }
+    });
 
   return (
-    <main>
-      {countries &&
-        countries.map((country) => (
-          <CountryCard key={crypto.randomUUID()} country={country} />
-        ))}
-    </main>
+    <>
+      <NavBar />
+      <main>
+        {isLoading && <p>Loading...</p>}
+        {isError && <p>{error}</p>}
+        {dataToRender && (
+          <>
+            {dataToRender
+              .filter((country) => country.continents.includes("Europe"))
+              .map((country) => (
+                <section>
+                  <h1>Europe</h1>
+                  <CountryCard key={crypto.randomUUID()} country={country} />
+                </section>
+              ))}
+            {dataToRender
+              .filter((country) => country.continents.includes("Asia"))
+              .map((country) => (
+                <section>
+                  <h1>Asia</h1>
+                  <CountryCard key={crypto.randomUUID()} country={country} />
+                </section>
+              ))}
+            {dataToRender
+              .filter((country) => country.continents.includes("Africa"))
+              .map((country) => (
+                <section>
+                  <h1>Africa</h1>
+                  <CountryCard key={crypto.randomUUID()} country={country} />
+                </section>
+              ))}
+            {dataToRender
+              .filter((country) => country.continents.includes("Oceania"))
+              .map((country) => (
+                <section>
+                  <h1>Oceania</h1>
+                  <CountryCard key={crypto.randomUUID()} country={country} />
+                </section>
+              ))}
+            {dataToRender
+              .filter((country) => country.continents.includes("South America"))
+              .map((country) => (
+                <section>
+                  <h1>South America</h1>
+                  <CountryCard key={crypto.randomUUID()} country={country} />
+                </section>
+              ))}
+            {dataToRender
+              .filter((country) => country.continents.includes("North America"))
+              .map((country) => (
+                <section>
+                  <h1>North America</h1>
+                  <CountryCard key={crypto.randomUUID()} country={country} />
+                </section>
+              ))}
+            {dataToRender
+              .filter((country) => country.continents.includes("Antarctica"))
+              .map((country) => (
+                <section>
+                  <h1>Antarcica</h1>
+                  <CountryCard key={crypto.randomUUID()} country={country} />
+                </section>
+              ))}
+          </>
+        )}
+        {/* <section id="Asia">
+        <h1>Rest</h1>
+        {countries &&
+          countries
+            // .filter((country) => country.continents.includes("Asia"))
+            .map((country) => (
+              <CountryCard key={crypto.randomUUID()} country={country} />
+            ))}
+      </section> */}
+      </main>
+    </>
   );
 }
 
