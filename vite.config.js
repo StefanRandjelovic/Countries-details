80d61787b394d80cb60ac32e7098eee69c30fcea
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-04-03 23:16:34 +0200

    Add additional import alias.

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       { find: "@components", replacement: "/src/Components" },
       { find: "@style", replacement: "/src/Styles" },
       { find: "@store", replacement: "/src/GlobalStore" },
-      // {find:'',replacement:''},
+      { find: "@svg", replacement: "/src/Svg" },
     ],
   },
 });
