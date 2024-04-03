6040ab512a30fac0f22896f8cf63138a13ae57b5
Stefan Randjelovic <stefanrandjelovic413@gmail.com>
2024-04-03 23:28:48 +0200

    Reconfigure the whole App component, add scroll display, back to top tag and scroll info tag.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 // DEV DEPENDENCIES
 import { useQuery } from "@tanstack/react-query";
+import { useState, useEffect } from "react";
 
 // HELPER FUNCTIONS
-import { handleFetch } from "@helpers/helper_functions";
+import { handleFetch, handleScroll } from "@helpers/helper_functions.js";
 
 // COMPONENTS
 import CountryCard from "@components/CountryCard";
@@ -11,9 +12,12 @@ import NavBar from "@components/NavBar";
 // GLOBAL VARIABLE
 import { searchFilter } from "@store/zustandStore";
 
+// SVG
+import Arrow from "@svg/arrow.svg";
+
 function App() {
   // GLOBAL STATE
-  const { inputValue, setInputValue } = searchFilter();
+  const { inputValue } = searchFilter();
 
   // FETCH COUNTRIES
   const { data, isError, isLoading, error } = useQuery({
@@ -21,7 +25,6 @@ function App() {
     queryFn: handleFetch,
   });
 
-  console.log(data);
   const dataToRender =
     data &&
     data.filter((country) => {
@@ -34,81 +37,54 @@ function App() {
       }
     });
 
+  // SCROLLING
+  const [displayCount, setDisplayCount] = useState(12);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => handleScroll(setDisplayCount));
+    return () =>
+      window.removeEventListener("scroll", () => handleScroll(setDisplayCount));
+  }, []);
+
   return (
     <>
       <NavBar />
       <main>
-        {isLoading && <p>Loading...</p>}
-        {isError && <p>{error}</p>}
+        {isLoading && <p id="loading">Loading...</p>}
+        {isError && <p id="error">{error}</p>}
         {dataToRender && (
           <>
             {dataToRender
-              .filter((country) => country.continents.includes("Europe"))
+              .sort((a, b) => {
+                if (a.name.common < b.name.common) {
+                  return -1;
+                } else {
+                  return 1;
+                }
+              })
+              .slice(0, displayCount)
+              .filter((country) => country.name.common !== "Kosovo")
               .map((country) => (
-                <section>
-                  <h1>Europe</h1>
-                  <CountryCard key={crypto.randomUUID()} country={country} />
-                </section>
-              ))}
-            {dataToRender
-              .filter((country) => country.continents.includes("Asia"))
-              .map((country) => (
-                <section>
-                  <h1>Asia</h1>
-                  <CountryCard key={crypto.randomUUID()} country={country} />
-                </section>
-              ))}
-            {dataToRender
-              .filter((country) => country.continents.includes("Africa"))
-              .map((country) => (
-                <section>
-                  <h1>Africa</h1>
-                  <CountryCard key={crypto.randomUUID()} country={country} />
-                </section>
-              ))}
-            {dataToRender
-              .filter((country) => country.continents.includes("Oceania"))
-              .map((country) => (
-                <section>
-                  <h1>Oceania</h1>
-                  <CountryCard key={crypto.randomUUID()} country={country} />
-                </section>
-              ))}
-            {dataToRender
-              .filter((country) => country.continents.includes("South America"))
-              .map((country) => (
-                <section>
-                  <h1>South America</h1>
-                  <CountryCard key={crypto.randomUUID()} country={country} />
-                </section>
-              ))}
-            {dataToRender
-              .filter((country) => country.continents.includes("North America"))
-              .map((country) => (
-                <section>
-                  <h1>North America</h1>
-                  <CountryCard key={crypto.randomUUID()} country={country} />
-                </section>
-              ))}
-            {dataToRender
-              .filter((country) => country.continents.includes("Antarctica"))
-              .map((country) => (
-                <section>
-                  <h1>Antarcica</h1>
-                  <CountryCard key={crypto.randomUUID()} country={country} />
-                </section>
+                <CountryCard key={crypto.randomUUID()} country={country} />
               ))}
           </>
         )}
-        {/* <section id="Asia">
-        <h1>Rest</h1>
-        {countries &&
-          countries
-            // .filter((country) => country.continents.includes("Asia"))
-            .map((country) => (
-              <CountryCard key={crypto.randomUUID()} country={country} />
-            ))}
-      </section> */}
+        {dataToRender &&
+          dataToRender.filter((country) => country.name.common !== "Kosovo")
+            .length == 0 && (
+            <p id="noCountry">"There are no countries to display"</p>
+          )}
+        <div id="top-container" title="Back to the top">
+          <a href="#" id="top">
+            <img src={Arrow} />
+          </a>
+        </div>
+        <div id="info">
+          <p>!</p>
+          <p id="hidden-info">
+            You can scroll inside a country card if its contents are hidden.
+          </p>
+        </div>
       </main>
     </>
   );
